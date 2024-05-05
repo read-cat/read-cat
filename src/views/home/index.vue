d05994c711dc1f27466bdbe237cbff008d16e2fc
Pandaer13 <1345514736@qq.com>
2024-05-05 23:14:24 +0800

    其他修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,16 @@
 <script setup lang="ts">
-import {
-  ElButton
-} from 'element-plus';
 import { onMounted } from 'vue';
 import { useScrollTopStore } from '../../store/scrolltop';
-import { EventCode } from '../../../events';
 import { PagePath } from '../../core/window';
 const { pageScrollTop } = useScrollTopStore();
 onMounted(() => {
   pageScrollTop(PagePath.HOME);
 });
 
-const full = () => {
-  GLOBAL_IPC.send(EventCode.ASYNC_WINDOW_SET_FULLSCREEN, true);
-}
 </script>
 
 <template>
   <div id="home" class="container">
-    <ElButton @click="full">全屏</ElButton>
     <div>home</div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import SvgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
   plugins: [
     vue(),
     electron({
