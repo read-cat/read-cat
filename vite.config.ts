0e098d96811448f22fce0f743eac8d9138d5b41a
Pandaer13 <1345514736@qq.com>
2024-05-05 23:21:31 +0800

    其他修改

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import SvgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: true
-  },
   plugins: [
     vue(),
     electron({
