021fb4167f8862fb4af88462147dd2a45154c41e
moomewa <1345514736@qq.com>
2024-09-13 22:57:23 +0800

    其他修改

--- src/components/settings/components/help/index.vue
+++ src/components/settings/components/help/index.vue
@@ -26,47 +26,49 @@ export default {
     </header>
     <main>
       <table>
-        <tr>
-          <td>版本</td>
-          <td class="version">
-            <span>{{ `${version}${branch === 'dev' ? '.' + date : ''}` }}</span>
-            <div>
-              <ElLink type="primary" :underline="false" @click="update(true)">检查更新</ElLink>
-              <ElLink type="primary" :underline="false" @click="getUpdateLog">更新日志</ElLink>
-              <ElLink type="primary" :underline="false" @click="exportLog">导出日志</ElLink>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>作者</td>
-          <td>Moomew</td>
-        </tr>
-        <tr>
-          <td>官网</td>
-          <td>
-            <ElLink href="https://read-cat.top" target="_blank">https://read-cat.top</ElLink>
-          </td>
-        </tr>
-        <tr>
-          <td>提交</td>
-          <td>{{ commit }}</td>
-        </tr>
-        <tr>
-          <td>Chrome</td>
-          <td>{{ chrome }}</td>
-        </tr>
-        <tr>
-          <td>Electron</td>
-          <td>{{ electron }}</td>
-        </tr>
-        <tr>
-          <td>Node.js</td>
-          <td>{{ node }}</td>
-        </tr>
-        <tr>
-          <td>V8</td>
-          <td>{{ v8 }}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>版本</td>
+            <td class="version">
+              <span>{{ `${version}${branch === 'dev' ? '.' + date : ''}` }}</span>
+              <div>
+                <ElLink type="primary" :underline="false" @click="update(true)">检查更新</ElLink>
+                <ElLink type="primary" :underline="false" @click="getUpdateLog">更新日志</ElLink>
+                <ElLink type="primary" :underline="false" @click="exportLog">导出日志</ElLink>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>作者</td>
+            <td>Moomew</td>
+          </tr>
+          <tr>
+            <td>官网</td>
+            <td>
+              <ElLink href="https://read-cat.top" target="_blank">https://read-cat.top</ElLink>
+            </td>
+          </tr>
+          <tr>
+            <td>提交</td>
+            <td>{{ commit }}</td>
+          </tr>
+          <tr>
+            <td>Chrome</td>
+            <td>{{ chrome }}</td>
+          </tr>
+          <tr>
+            <td>Electron</td>
+            <td>{{ electron }}</td>
+          </tr>
+          <tr>
+            <td>Node.js</td>
+            <td>{{ node }}</td>
+          </tr>
+          <tr>
+            <td>V8</td>
+            <td>{{ v8 }}</td>
+          </tr>
+        </tbody>
       </table>
     </main>
   </div>
@@ -116,10 +118,12 @@ export default {
         &>span {
           user-select: text;
         }
+
         &>div {
           display: flex;
           align-items: center;
         }
+
         :deep(.el-link) {
           color: var(--rc-theme-color);
           transition: scale 0.3s ease;
